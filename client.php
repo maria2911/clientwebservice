e7b1e663b0b7b185c7e46cdb7f1595dee0675672
jumaidi <jumaidi10@gmail.com>
2019-02-27 10:12:15 +0800

    clientWeb

---
+++ client.php
@@ -0,0 +1,17 @@
+<?php
+// cara nak access webservice 
+// buat di composer
+
+// client webservice
+//to call film web service
+require 'vendor/autoload.php';
+$client = new GuzzleHttp\Client();
+$result = $client->request('GET',
+'http://localhost:8080/webservice/sakila/film_svc.php');
+
+$films = json_decode($result->getBody()->getContents());
+
+foreach ($films as $film) 
+{
+	echo $film->title . '<br>';
+}
